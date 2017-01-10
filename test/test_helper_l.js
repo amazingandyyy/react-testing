1722e5ca18596359e96cd23b21d0ca7aa84a104e
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-09 23:59:22 -0800

    add jsdom

--- test/test_helper_l.js
+++ test/test_helper_l.js
@@ -1,7 +1,7 @@
-
+import jsdom from 'jsdom';
 
 // Set up testing enviroment to run like a browser in the command line
-
+global.document = jsdom.jsdom('<!doctype html> <html> </html> <body></body>')
 
 // build 'renderComponent' helper that should render a given react class
 
